c52f3de952a30a57f24ae493bc88076185c81f29
Nash6643 <omarnash619@gmail.com>
2024-10-06 11:17:01 +0300

    Update Inputting a  specific number of elements.c

--- README.c
+++ README.c
@@ -1,2 +1,48 @@
-# NewCodingBot
-C programming-A new repo
+    #include<stdio.h>
+int main(){
+
+int num1;
+int num2;
+int num3;
+
+int count;
+
+printf("Enter number one: ");
+
+scanf("%d",&num1);
+
+printf("Enter number two: ");
+
+scanf("%d",&num2);
+
+printf("Enter number three: ");
+
+scanf("%d",&num3);
+
+if (num1 != num2 && num1 != num3){
+    count++;
+}
+if (num2 != num1 && num2 != num3){
+
+    count++;;
+}
+if (num3 != num1 && num3 != num2){
+
+    count++;;
+}
+
+if (count == 0){
+    count = 1;
+}else if (count == 1){
+    count = 2;
+}else {
+    count = 3;
+}
+
+printf("Number of unique integers is: %d\n", count);
+
+
+
+
+    return 0;
+}
